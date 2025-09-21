f54bf796cd0d878a7cb656bd08b8143db1c71567
Mithun R Naik <mithunrnaik@outlook.com>
2025-09-21 11:29:20 +0530

    Test CI/CD deployment v8

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-kubectl apply --validate=false -f k8s/deployment.yaml
-kubectl apply --validate=false -f k8s/service.yaml
-kubectl apply --validate=false -f k8s/ingress.yaml
-
